0d49d263d5e072b8c51ec26ea1f8537d4da33ff3
Prakash <y509447@softwareag.com>
2021-03-09 01:23:28 +0530

    Added some custom components in the sign in form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shoppage.component';
 import Header from './components/header/header.component';
+import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up-page.component';
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
             <Switch>
                 <Route exact path="/" component={HomePage} />
                 <Route exact path="/shop" component={ShopPage} />
+                <Route exat path="/signIn" component={SignInSignUpPage} />
             </Switch>
         </div>
     );
